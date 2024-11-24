dd3a4646f58a5e56866fedaeba80ee167f3c0956
es-sai-fi <jhormangomez2005@outlook.com>
2024-11-23 20:29:02 -0500

    Changes to course app

--- backend/apps/session/urls.py
+++ backend/apps/session/urls.py
@@ -3,7 +3,7 @@ from .views import Session, exit, create_session, course_sessions, show_session
 urlpatterns = [
     path ('sessions/', course_sessions,name='course_sessions'),
     path ('logout/', exit,name='exit'),
-    path('create_session/', create_session,name='create_session'),
-    path('s:<int:aula_id>/', show_session,name='session_online'),
+    path('create_session/', create_session, name='create_session'),
+    path('s:<int:aula_id>/', show_session, name='session_online'),
 
 ]
