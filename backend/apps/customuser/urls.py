9f60a79cb59abdf5b1a916ce7815a146e74ee94c
es-sai-fi <jhormangomez2005@outlook.com>
2024-12-12 23:00:43 -0500

    CustomUser api

--- backend/apps/customuser/urls.py
+++ backend/apps/customuser/urls.py
@@ -1,12 +1,10 @@
-
-from django.urls import path, include
-
-from .views import home, user_login_view, user_singup_view, select_user_type_view,logout_view
+from django.urls import path
+from .views import LoginView, LogoutView, RegisterView, HomeView
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('logout/', logout_view, name='logout'),
-    path('login/', user_login_view, name='login'),
-    path('select_user_type/', select_user_type_view, name='select_user_type'),
-    path('signup/', user_singup_view, name='signup'),
+    path('', HomeView.as_view(), name='home'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', RegisterView.as_view(), name='signup'),
 ]
+
