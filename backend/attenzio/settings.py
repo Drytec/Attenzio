4d74a04ac2b8af31995f312b6c77d3387a76c3ba
es-sai-fi <jhormangomez2005@outlook.com>
2024-12-13 09:35:21 -0500

    CustomUser, Course, Session apis

--- backend/attenzio/settings.py
+++ backend/attenzio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'apps.customuser',
     'apps.session',
@@ -62,6 +63,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'attenzio.urls'
